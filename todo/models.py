59844ca6676259550a7884819cc0abc5b360005e
Mojtaba <mji741258963@gmail.com>
2023-12-06 22:24:33 +0330

    updated the TaskModel with 'null=True' for date and about

--- todo/models.py
+++ todo/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class TasksModel(models.Model):
     task = models.CharField(max_length=255)
     descriptions = models.TextField(null=True, blank=True)
-    date = models.DateTimeField(blank=True)
+    date = models.DateTimeField(blank=True, null=True)
     durations = models.IntegerField(blank=True, null=True)
     created = models.DateTimeField(auto_now=True)
     done = models.BooleanField(default=False, blank=True)
-    about = models.TextField(blank=True, default='')
+    about = models.TextField(blank=True, null=True)
 
     class Meta:
         ordering = ['created']
