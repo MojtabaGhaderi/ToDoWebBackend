d3fd0b5530d1b3014a608853eee8d45916e9bd8a
Mojtaba <mji741258963@gmail.com>
2023-12-07 22:33:43 +0330

    created serializers

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,5 +1,28 @@
 from rest_framework import serializers
-from todo.models import TasksModel
+from todo.models import TasksModel, ProfilePictureModel
+from django.contrib.auth.models import User
+
+
+class ProfileUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfilePictureModel
+        fields = ['profile_pic']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile_pic = ProfileUserSerializer(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'profile_pic']
+
+    def create(self, validated_data):
+        profile_pic_data = validated_data.pop('profile_pic', None)
+        user = User.objects.create_user(**validated_data)
+
+        if profile_pic_data:
+            ProfilePictureModel.objects.create_user(**profile_pic_data)
+        return user
 
 
 class TasksSerializer(serializers.ModelSerializer):
