f2c9dc6f2c8bb3dc0d2c1585d39ea7b7aef12c43
Mojtaba <mji741258963@gmail.com>
2023-12-08 01:53:14 +0330

    added a LoginAPIView class

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,24 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from django.contrib.auth import authenticate, login
+
 from .serializers import TasksSerializer, AboutSerializer, ProfileUserSerializer, UserSerializer
 from .models import TasksModel
+
+
+class LoginAPIView(APIView):
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request.user)
+            return Response({'detail': 'authentication successfull.'})
+        return Response({'detail': 'invalid'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class UserCreateView(generics.CreateAPIView):
