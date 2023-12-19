719aad6b9b8df83df3c53c9f446ae2a6e3926102
Mojtaba <mji741258963@gmail.com>
2023-12-19 13:03:42 +0330

    wrote permissions

--- todo/permissions.py
+++ todo/permissions.py
@@ -1,2 +1,25 @@
 from rest_framework.permissions import BasePermission
 
+from todo.models import GroupModel
+
+
+class IsSelf(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+
+class IsGroupHead(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.creator == request.user
+
+
+class IsGroupAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.admin == request.user
+
+
+class IsInGroup(BasePermission):
+    def has_permission(self, request, view):
+        group_id = view.kwargs.get('pk')
+        user = request.user
+        return GroupModel.objects.filter(id=group_id, members=user).exist()
